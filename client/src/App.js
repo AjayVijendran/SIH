7d2f42ed9336e37cd7417b6f662e84cc9a2b9e4f
Gopal Singh Rajput <87819913+GopalSinghRajput@users.noreply.github.com>
2023-09-23 01:42:24 +0530

    landing_page

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,21 @@ import Loginpage from "./components/login-page";
 import Userprofile from './components/userprofile/Userprofile'
 import LandingPage from "./components/LandingPage";
 import React from 'react';
+import Homepage from './components/home-page/homepage'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from "./components/home";
 import {ToastContainer} from 'react-toastify'
 import PrivateRoute from "./components/privateRoute";
 import 'react-toastify/dist/ReactToastify.css'
-import Homepage from "./components/home-page/homepage";
-
 function App() {
     return (
         <BrowserRouter>
             <ToastContainer position="top-center"/>
             <Routes>
-                <Route path = "/home" element = {<Homepage />}></Route>
+            
                 <Route path = "/landing" element = {<LandingPage />}></Route>
                 <Route path="/auth" element={<Loginpage/>}></Route>
                 <Route path="" element={<PrivateRoute/>}>
-                    <Route path="/home" element={<Home/>}></Route>
+                <Route path = "/home" element = {<Homepage />}></Route>
                     <Route path="/profile" element={<Userprofile/>}></Route>
                 </Route>
                 
